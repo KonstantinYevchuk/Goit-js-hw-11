c3636e92e9b16e8860d3a526e768529ba87dbbf6
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2023-01-16 16:56:49 +0200

    working day3

--- src/fetchFhotos.js
+++ src/fetchFhotos.js
@@ -1,9 +1,9 @@
 // https://pixabay.com/api/?key=32830280-cd5d8cae887a4565b5001e89e&q=yellow+flowers&image_type=photo
-async function fetchMake(name) {
+async function fetchMake(name, page=1) {
     const BASE_URL = 'https://pixabay.com/api/';
     const KEY = '32830280-cd5d8cae887a4565b5001e89e';
     
-    const search = await fetch(`${BASE_URL}?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`);
+    const search = await fetch(`${BASE_URL}?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&${page}`);
     if(!search.ok) {
         throw new Error(search.textStatus)
     }
--- src/index.js
+++ src/index.js
@@ -10,26 +10,64 @@ import Notiflix from 'notiflix';
 const axios = require('axios').default;
 import { fetchMake } from './fetchFhotos';
 
+const galleryEl = document.querySelector('.gallery');
 const formEl = document.querySelector("#search-form");
+const loadBtn = document.querySelector('.load-more')
 formEl.addEventListener('submit', submitEvent);
 
-function submitEvent(evt) {
+let page = 1;
+async function submitEvent(evt) {
     evt.preventDefault()
+    
     const { searchQuery
-    } = evt.currentTarget.trim();
-    console.log(searchQuery.value);
-    fetchMake(searchQuery.value).then(resp => {
+    } = evt.currentTarget;
+    galleryEl.innerHTML = "";
+    // evt.currentTarget.reset();
+    // console.log(searchQuery.value);
+    try {
+        const resultFetch = await fetchMake(searchQuery.value.trim())
+        console.log(resultFetch.hits);
+        if(resultFetch.hits.length === 0) {
+            Notiflix.Notify.failure('"Sorry, there are no images matching your search query. Please try again."')
+        }
+
+        createMarkup(resultFetch.hits)
+        loadBtn.hidden = false;
         
-        console.log(resp);
-    })
-    .catch(error => {
-        console.log(error);
-    })
+    } catch(error) {
+     console.log(error);
+    }
+    
 }
 
+loadBtn.addEventListener('click', clickEvent);
+
+async function clickEvent(evt) {
+    evt = await fetchMake(); 
+}
+
+
+
 function createMarkup(arr) {
-    const murkup = arr.map(({}) => 
-    ``) 
-        
+    const markup = arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) =>  
+    `<div class="photo-card">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" width="250px" height="180px" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads ${downloads}</b>
+      </p>
+    </div>
+  </div>`).join('');
+
+   galleryEl.insertAdjacentHTML('beforeend', markup);     
     
 }
